fa98da1f623ae427be628d3a9db10d2d7b05091a
cireeijie <ericjohnariate@gmail.com>
2023-10-02 04:09:06 +0200

    updated authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
+'use server'
+
 import bcrypt from 'bcrypt';
-import NextAuth, { NextAuthOptions} from 'next-auth';
+import NextAuth, { AuthOptions} from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -7,7 +9,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import prisma from '@/app/libs/prismadb';
 
-const authOptions: NextAuthOptions = {
+const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
@@ -61,4 +63,4 @@ const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
